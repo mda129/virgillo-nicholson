6f951ef4236977d6b3b6b454ffcc7c06640309d8
Michael Ansell <mda129@gmail.com>
2025-12-04 14:22:46 -0500

    New Try

--- js/script.js
+++ js/script.js
@@ -12,14 +12,10 @@ jQuery(function ($) {
     /* Fixed header
     /* ----------------------------------------------------------- */
     $(window).on('scroll', function () {
-
-        // fixedHeader on scroll
         function fixedHeader() {
             var headerTopBar = $('.top-bar').outerHeight();
             var headerOneTopSpace = $('.header-one .logo-area').outerHeight();
-
             var headerOneELement = $('.header-one .site-navigation');
-            var headerTwoELement = $('.header-two .site-navigation');
 
             if ($(window).scrollTop() > headerTopBar + headerOneTopSpace) {
                 $(headerOneELement).addClass('navbar-fixed');
@@ -28,63 +24,11 @@ jQuery(function ($) {
                 $(headerOneELement).removeClass('navbar-fixed');
                 $('.header-one').css('margin-bottom', 0);
             }
-            if ($(window).scrollTop() > headerTopBar) {
-                $(headerTwoELement).addClass('navbar-fixed');
-                $('.header-two').css('margin-bottom', headerTwoELement.outerHeight());
-            } else {
-                $(headerTwoELement).removeClass('navbar-fixed');
-                $('.header-two').css('margin-bottom', 0);
-            }
         }
         fixedHeader();
-
-
-        // Count Up
-        function counter() {
-            var oTop;
-            if ($('.counterUp').length !== 0) {
-                oTop = $('.counterUp').offset().top - window.innerHeight;
-            }
-            if ($(window).scrollTop() > oTop) {
-                $('.counterUp').each(function () {
-                    var $this = $(this),
-                        countTo = $this.attr('data-count');
-                    $({
-                        countNum: $this.text()
-                    }).animate({
-                        countNum: countTo
-                    }, {
-                        duration: 1000,
-                        easing: 'swing',
-                        step: function () {
-                            $this.text(Math.floor(this.countNum));
-                        },
-                        complete: function () {
-                            $this.text(this.countNum);
-                        }
-                    });
-                });
-            }
-        }
-        counter();
-
-
-        // scroll to top btn show/hide
-        function scrollTopBtn() {
-            var scrollToTop = $('#back-to-top'),
-                scroll = $(window).scrollTop();
-            if (scroll >= 50) {
-                scrollToTop.fadeIn();
-            } else {
-                scrollToTop.fadeOut();
-            }
-        }
-        scrollTopBtn();
     });
 
-
     $(document).ready(function () {
-
         // navSearch show/hide
         function navSearch() {
             $('.nav-search').on('click', function () {
@@ -104,13 +48,11 @@ jQuery(function ($) {
                         height: 'toggle'
                     }, 300);
                 });
-
                 var navbarHeight = $('.site-navigation').outerHeight();
                 $('.site-navigation .navbar-collapse').css('max-height', 'calc(100vh - ' + navbarHeight + 'px)');
             }
         }
         navbarDropdown();
-
 
         // back to top
         function backToTop() {
@@ -124,11 +66,10 @@ jQuery(function ($) {
         }
         backToTop();
 
-
         // banner-carousel (FIXED: Added fade: true)
         function bannerCarouselOne() {
             $('.banner-carousel.banner-carousel-1').slick({
-                fade: true,  // <--- FIXES SLIDER GLITCH
+                fade: true,
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 autoplay: true,
@@ -141,24 +82,6 @@ jQuery(function ($) {
             $('.banner-carousel.banner-carousel-1').slickAnimation();
         }
         bannerCarouselOne();
-
-
-        // banner Carousel Two
-        function bannerCarouselTwo() {
-            $('.banner-carousel.banner-carousel-2').slick({
-                fade: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                autoplay: true,
-                dots: false,
-                speed: 600,
-                arrows: true,
-                prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
-                nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
-            });
-        }
-        bannerCarouselTwo();
-
 
         // pageSlider
         function pageSlider() {
@@ -176,8 +99,7 @@ jQuery(function ($) {
         }
         pageSlider();
 
-
-        // Shuffle js filter and masonry
+        // Shuffle js filter
         function projectShuffle() {
             if ($('.shuffle-wrapper').length !== 0) {
                 var Shuffle = window.Shuffle;
@@ -200,58 +122,6 @@ jQuery(function ($) {
         }
         projectShuffle();
 
-
-        // testimonial carousel
-        function testimonialCarousel() {
-            $('.testimonial-slide').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                speed: 600,
-                arrows: false
-            });
-        }
-        testimonialCarousel();
-
-
-        // team carousel
-        function teamCarousel() {
-            $('.team-slide').slick({
-                dots: false,
-                infinite: false,
-                speed: 300,
-                slidesToShow: 4,
-                slidesToScroll: 2,
-                arrows: true,
-                prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
-                nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>',
-                responsive: [{
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3
-                        }
-                    },
-                    {
-                        breakpoint: 768,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
-                        }
-                    },
-                    {
-                        breakpoint: 481,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                ]
-            });
-        }
-        teamCarousel();
-
-
         // media popup
         function mediaPopup() {
             $('.gallery-popup').colorbox({
@@ -259,15 +129,7 @@ jQuery(function ($) {
                 transition: 'slideshow',
                 innerHeight: '500'
             });
-            $('.popup').colorbox({
-                iframe: true,
-                innerWidth: 600,
-                innerHeight: 400
-            });
         }
         mediaPopup();
-
     });
-
-
 });
